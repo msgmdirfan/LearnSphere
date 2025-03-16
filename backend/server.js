59f4fbb4a4da7b11b23c0e7f52882789874ac096
msgmdirfan <msgmdirfan@gmail.com>
2025-03-16 23:19:20 +0530

    added to mongodb atlass but not work correctley

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,8 @@ app.use(bodyParser.json());
 
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
+
   .then(() => console.log("MongoDB Connected Successfully"))
   .catch((err) => console.error("MongoDB Connection Error:", err));
 
